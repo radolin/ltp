de96fc1497b81ccd6acd17dcd561f91be1cad35f
Radoslav Kolev <radoslav.kolev@suse.com>
2020-10-28 16:46:08 +0200

    Change according to feedback from metan on IRC
    Use tabs for indentation
    Close duped FD to not leak it
    Don't use strerror()

--- testcases/kernel/syscalls/dup/dup01.c
+++ testcases/kernel/syscalls/dup/dup01.c
@@ -7,34 +7,36 @@
 #include "tst_test.h"
 
 static int fd;
+static int dup_fd;
 
 static void verify_dup(void)
 {
-		TEST(dup(fd));
+	TEST(dup_fd = dup(fd));
 
-		if (TST_RET == -1) {
-			tst_res(TFAIL | TERRNO, "dup(%d) Failed, errno=%d : %s",
-                fd, TST_ERR, strerror(TST_ERR));
-		} else {
-			tst_res(TPASS, "dup(%d) returned %ld",
-				 fd, TST_RET);
-        }
+	if (TST_RET == -1) {
+		tst_res(TFAIL | TERRNO , "dup(%d) Failed, %s",
+			fd);
+	} else {
+		tst_res(TPASS, "dup(%d) returned %ld",
+			fd, TST_RET);
+		SAFE_CLOSE(dup_fd);
+	}
 }
 
 static void setup(void)
 {
-    fd = SAFE_OPEN("dupfile", O_RDWR | O_CREAT, 0700);
+	fd = SAFE_OPEN("dupfile", O_RDWR | O_CREAT, 0700);
 }
 
 static void cleanup(void)
 {
-    if (fd > 0)
-        SAFE_CLOSE(fd);
+	if (fd > 0)
+		SAFE_CLOSE(fd);
 }
 
 static struct tst_test test = {
 	.test_all = verify_dup,
-    .setup = setup,
+	.setup = setup,
 	.cleanup = cleanup,
-    .needs_tmpdir = 1,
+	.needs_tmpdir = 1,
 };
